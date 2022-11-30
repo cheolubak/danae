7aaf973a918ee49b79163749aea9f8d2511b741a
Cheolu.B <dev.cheolubak@gmail.com>
2022-11-30 22:02:39 +0900

    Fix: nest config

--- server/src/database.module.ts
+++ server/src/database.module.ts
@@ -1,20 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import databaseConfig from './config/database.config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'mariadb',
-      host: process.env.DATABASE_HOST,
-      port: +(process.env.DATABASE_PORT || 3306),
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      entities: [__dirname + './**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
-  ],
+  imports: [TypeOrmModule.forRootAsync({ useFactory: databaseConfig })],
 })
 export class DatabaseModule {}
